309923b764c2293f274f93ab2315ee8f77512ad1
Dinesh Weerapurage <xydinesh@gmail.com>
2017-01-07 06:29:45 +0000

    adding ubuntu-16.04 instructions for installing puppetserver

---
+++ ubuntu-16.04/puppetserverinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Enable apt package management system to add puppetlabs repositories
+# Follow documentation from:
+# https://docs.puppet.com/puppet/latest/puppet_collections.html#apt-based-systems
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+sudo dpkg -i puppetlabs-release-pc1-xenial.deb
+sudo apt update
+
+# install puppetserver
+sudo apt-get install -y puppetserver
